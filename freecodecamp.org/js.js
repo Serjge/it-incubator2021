904f527057aaa0e9c0f7ed2e765eb032976acb39
Serjge <serjge@yandex.ru>
2021-10-08 12:59:54 +0300

    Rename file and passed 20-29 test

---
+++ freecodecamp.org/js.js
@@ -0,0 +1,202 @@
+// 01 test
+
+// one line comment
+
+/*
+multi-line comment
+*/
+
+//02 test
+
+var myName;
+
+//03 test
+
+// Setup
+var a;
+a = 7;
+// Only change code below this line
+
+//04 test
+
+// Setup
+var a;
+a = 7;
+var b;
+b = a;
+// Only change code below this line
+
+//05 test
+
+var a = 9;
+
+//06 test
+
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = 'I am a';
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + ' String!';
+
+// 07 test
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = 'A String';
+titleCaseOver = 9000;
+
+//08 test
+
+var sum = 10 + 10;
+
+// 09 test
+
+var difference = 45 - 33;
+
+//10 test
+
+var product = 8 * 10;
+
+//11 test
+
+var quotient = 66 / 33;
+
+//12 test
+
+var myVar = 87;
+
+// Only change code below this line
+myVar++;
+
+//13 test
+
+var myVar = 11;
+
+// Only change code below this line
+myVar--;
+
+//14 test
+
+var myDecimal = 5.7;
+
+// Only change code below this line
+
+//15 test
+
+var product = 2.0 * 2.5;
+
+// 16 test
+
+var quotient = 4.4 / 2.0; // Change this line
+
+// 17 test
+
+// Only change code below this line
+
+var remainder = 11 % 3;
+
+// 18 test
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+// 19 test
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
+// 20 test
+
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only change code below this line
+a *= a;
+b *= 3;
+c *= 10;
+
+// 21 test
+
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+// 22 test
+
+var myFirstName = 'Sergey';
+var myLastName = 'Dudkin';
+
+// 23 test
+
+var myStr = 'I am a "double quoted" string inside "double quotes".';
+// Change this line
+
+// 24 test
+
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+// 25 test
+
+var myStr; // Change this line
+
+myStr = 'FirstLine\n\t\\SecondLine\nThirdLine';
+
+// 26 test
+
+var myStr; // Change this line
+
+myStr = 'This is the start.' + ' ' + 'This is the end.';
+
+// 27 test
+
+// Only change code below this line
+
+var myStr = 'This is the first sentence.';
+
+myStr += ' This is the second sentence.';
+
+// 28 test
+
+// Only change code below this line
+var myName = 'Sergey';
+var myStr = 'My name is' + myName + 'and I am well!';
+
+// 29 test
+
+// Change code below this line
+
+var someAdjective = 'best';
+var myStr = 'Learning to code is ';
+
+myStr += someAdjective;
+
+// 30 test
+
+// 31 test
