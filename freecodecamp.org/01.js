a4d916915f818123f7057fad4016d30385d04a3c
Serjge <serjge@yandex.ru>
2021-10-08 12:01:07 +0300

    passed 10-19 test

--- freecodecamp.org/01.js
+++ freecodecamp.org/01.js
@@ -61,3 +61,79 @@ var sum = 10 + 10;
 // 09 test
 
 var difference = 45 - 33;
+
+//10 test
+
+var product = 8 * 10;
+
+//11 test
+
+var quotient = 66 / 33;
+
+//12 test
+
+var myVar = 87;
+
+// Only change code below this line
+myVar++;
+
+//13 test
+
+var myVar = 11;
+
+// Only change code below this line
+myVar--;
+
+//14 test
+
+var myDecimal = 5.7;
+
+// Only change code below this line
+
+//15 test
+
+var product = 2.0 * 2.5;
+
+// 16 test
+
+var quotient = 4.4 / 2.0; // Change this line
+
+// 17 test
+
+// Only change code below this line
+
+var remainder = 11 % 3;
+
+// 18 test
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+// 19 test
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
+// 20 test
+
+// 21 test
+
+// 22 test
+
+// 23 test
+
+// 24 test
+
+// 25 test
