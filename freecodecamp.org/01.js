386f6bc216a9b8e554d53fbe2f4f272459d1b310
Serjge <serjge@yandex.ru>
2021-10-08 10:29:53 +0300

    passed 06 test

--- freecodecamp.org/01.js
+++ freecodecamp.org/01.js
@@ -29,3 +29,15 @@ b = a;
 //05 test
 
 var a = 9;
+
+//06 test
+
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = 'I am a';
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + ' String!';
