5e11927da447f9680ea2723b99a8f480082749bc
Sergey Dudkin <Serjge@yandex.ru>
2021-10-12 17:45:24 +0300

    passed freecodecamp 70-90 test

--- freecodecamp.org/js.js
+++ freecodecamp.org/js.js
@@ -686,22 +686,467 @@ testElse(4);
 
 // 70 test
 
+function testElseIf(val) {
+	if (val > 10) {
+		return "Greater than 10";
+	} else if (val < 5) {
+		return "Smaller than 5";
+	} else {
+		return "Between 5 and 10";
+	}
+}
+
+testElseIf(7);
+
 // 71 test
+
+function orderMyLogic(val) {
+	if (val < 5) {
+		return "Less than 5";
+	} else if (val < 10) {
+		return "Less than 10";
+	} else {
+		return "Greater than or equal to 10";
+	}
+}
+
+orderMyLogic(7);
 
 // 72 test
 
+function testSize(num) {
+	// Only change code below this line
+	if (num < 5) {
+		return "Tiny"
+	} else if (num < 10) {
+		return "Small"
+	} else if (num < 15) {
+		return "Medium"
+	} else if (num < 20) {
+		return "Large"
+	} else if (num >= 20) {
+		return "Huge"
+	}
+
+	return "Change Me";
+	// Only change code above this line
+}
+
+testSize(7);
+
 // 73 test
+
+var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+function golfScore(par, strokes) {
+	// Only change code below this line
+	if (strokes == 1) {
+		return names[0]
+	} else if (strokes <= par - 2) {
+		return names[1]
+	} else if (strokes == par - 1) {
+		return names[2]
+	} else if (strokes == par) {
+		return names[3]
+	} else if (strokes == par + 1) {
+		return names[4]
+	} else if (strokes == par + 2) {
+		return names[5]
+	} else if (strokes >= par + 3) {
+		return names[6]
+	}
+
+	return "Change Me";
+	// Only change code above this line
+}
+
+golfScore(5, 4);
 
 // 74 test
 
+function caseInSwitch(val) {
+	var answer = "";
+	// Only change code below this line
+	switch (val) {
+		case 1:
+			return "alpha";
+			break;
+		case 2:
+			return "beta";
+			break;
+		case 3:
+			return "gamma";
+			break;
+		case 4:
+			return "delta";
+			break;
+	}
+
+	// Only change code above this line
+	return answer;
+}
+
+caseInSwitch(1);
+
 // 75 test
+
+function switchOfStuff(val) {
+	var answer = "";
+	// Only change code below this line
+	switch (val) {
+		case "a":
+			return "apple";
+			break;
+		case "b":
+			return "bird"
+			break;
+		case "c":
+			return "cat";
+			break;
+		default:
+			return "stuff"
+			break;
+	}
+
+
+	// Only change code above this line
+	return answer;
+}
+
+switchOfStuff(1);
 
 // 76 test
 
+function sequentialSizes(val) {
+	var answer = "";
+	// Only change code below this line
+	switch (val) {
+		case 1:
+		case 2:
+		case 3:
+			answer = "Low";
+			break;
+		case 4:
+		case 5:
+		case 6:
+			answer = "Mid";
+			break;
+		case 7:
+		case 8:
+		case 9:
+			answer = "High"
+	}
+
+
+	// Only change code above this line
+	return answer;
+}
+
+sequentialSizes(1);
+
 // 77 test
+
+function chainToSwitch(val) {
+	var answer = "";
+	// Only change code below this line
+
+	switch (val) {
+		case "bob":
+			return "Marley"
+			break
+		case 42:
+			return "The Answer"
+			break
+		case 1:
+			return "There is no #1"
+			break
+		case 99:
+			return "Missed me by this much!"
+			break
+		case 7:
+			return "Ate Nine"
+			break
+	}
+	/*
+		if (val === "bob") {
+			answer = "Marley";
+		} else if (val === 42) {
+			answer = "The Answer";
+		} else if (val === 1) {
+			answer = "There is no #1";
+		} else if (val === 99) {
+			answer = "Missed me by this much!";
+		} else if (val === 7) {
+			answer = "Ate Nine";
+		}
+	*/
+	// Only change code above this line
+	return answer;
+}
+
+chainToSwitch(7);
 
 // 78 test
 
+function isLess(a, b) {
+	// Only change code below this line
+	return a < b;
+
+	/*
+	if (a < b) {
+		return true;
+	} else {
+		return false;
+	}
+	*/
+	// Only change code above this line
+}
+
+isLess(10, 15);
+
 // 79 test
 
+// Setup
+function abTest(a, b) {
+	// Only change code below this line
+	if (a < 0 || b < 0) {
+		return undefined
+	}
+
+	// Only change code above this line
+
+	return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+abTest(2, 2);
+
 // 80 test
+
+var count = 0;
+
+function cc(card) {
+	// Only change code below this line
+	switch (card) {
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+			count++;
+			break;
+		case 10:
+		case "A":
+		case "J":
+		case "K":
+		case "Q":
+			count--;
+			break;
+	}
+	var holdBet = "Hold"
+	if (count > 0) {
+		holdBet = "Bet"
+	}
+
+	return count + " " + holdBet;
+	// Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+// 81 test
+
+var myDog = {
+	// Only change code below this line
+	"name": "doges",
+	"legs": 4,
+	"tails": 10,
+	"friends": ["yes", "dog"]
+
+	// Only change code above this line
+};
+
+// 82 test
+
+// Setup
+var testObj = {
+	"hat": "ballcap",
+	"shirt": "jersey",
+	"shoes": "cleats"
+};
+
+// Only change code below this line
+
+var hatValue = testObj.hat;      // Change this line
+var shirtValue = testObj.shirt;    // Change this line
+
+// 83 test
+
+// Setup
+var testObj = {
+	"an entree": "hamburger",
+	"my side": "veggies",
+	"the drink": "water"
+};
+
+// Only change code below this line
+
+var entreeValue = testObj["an entree"];   // Change this line
+var drinkValue = testObj["the drink"];    // Change this line
+
+// 84 test
+
+// Setup
+var testObj = {
+	12: "Namath",
+	16: "Montana",
+	19: "Unitas"
+};
+
+// Only change code below this line
+
+var playerNumber = 16;       // Change this line
+var player = testObj[playerNumber];   // Change this line
+
+// 85 test
+
+// Setup
+var myDog = {
+	"name": "Coder",
+	"legs": 4,
+	"tails": 1,
+	"friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line
+
+myDog.name = "Happy Coder"
+
+// 86 test
+
+var myDog = {
+	"name": "Happy Coder",
+	"legs": 4,
+	"tails": 1,
+	"friends": ["freeCodeCamp Campers"]
+};
+
+myDog.bark = "woof"
+
+// 87 test
+
+// Setup
+var myDog = {
+	"name": "Happy Coder",
+	"legs": 4,
+	"tails": 1,
+	"friends": ["freeCodeCamp Campers"],
+	"bark": "woof"
+};
+
+// Only change code below this line
+
+delete myDog.tails
+
+// 88 test
+
+// Setup
+function phoneticLookup(val) {
+	var result = "";
+
+	// Only change code below this line
+	var lookup = {
+		"alpha": "Adams",
+		"bravo": "Boston",
+		"charlie": "Chicago",
+		"delta": "Denver",
+		"echo": "Easy",
+		"foxtrot": "Frank"
+	}
+	result = lookup[val]
+
+
+	// Only change code above this line
+	return result;
+}
+
+phoneticLookup("charlie");
+
+// 89 test
+
+function checkObj(obj, checkProp) {
+	// Only change code below this line
+	if (obj.hasOwnProperty(checkProp)) {
+		return obj[checkProp]
+	}
+	return "Not Found";
+	// Only change code above this line
+}
+
+// 90 test
+
+var myMusic = [
+	{
+		"artist": "Billy Joel",
+		"title": "Piano Man",
+		"release_year": 1973,
+		"formats": [
+			"CD",
+			"8T",
+			"LP"
+		],
+		"gold": true
+	},
+	{
+		"artist": "Billy Joel",
+		"title": "Piano Man",
+		"release_year": 1973,
+		"formats": [
+			"CD",
+			"8T",
+			"LP"
+		],
+		"gold": true
+	}
+];
+
+// 91 test
+
+// 92 test
+
+// 93 test
+
+// 94 test
+
+// 95 test
+
+// 96 test
+
+// 97 test
+
+// 98 test
+
+// 99 test
+
+// 100 test
+
+// 101 test
+
+// 102 test
+
+// 103 test
+
+// 104 test
+
+// 105 test
+
+// 106 test
+
+// 107 test
+
+// 108 test
+
+// 109 test
+
+// 110 test
+
+// 111 test
+
