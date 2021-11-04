4b0fcd627d89dc3527fb2d3c0c9886e06dba2a23
Serjge <serjge@yandex.ru>
2021-11-04 22:50:19 +0300

    Passed OOP test

---
+++ freecodecamp.org/Functional Programming.js
@@ -0,0 +1,48 @@
+//Test 01
+
+//Test 02
+
+//Test 03
+
+//Test 04
+
+//Test 05
+
+//Test 06
+
+//Test 07
+
+//Test 08
+
+//Test 09
+
+//Test 10
+
+//Test 11
+
+//Test 12
+
+//Test 13
+
+//Test 14
+
+//Test 15
+
+//Test 16
+
+//Test 17
+
+//Test 18
+
+//Test 19
+
+//Test 20
+
+//Test 21
+
+//Test 22
+
+//Test 23
+
+//Test 24
+
--- freecodecamp.org/Object Oriented Programming.js
+++ freecodecamp.org/Object Oriented Programming.js
@@ -1,15 +1,15 @@
 //Test1
 
 let dog = {
-  name: 'Raks',
-  numLegs: 4,
+    name: 'Raks',
+    numLegs: 4,
 };
 
 //Test2
 
 let dog = {
-  name: 'Spot',
-  numLegs: 4,
+    name: 'Spot',
+    numLegs: 4,
 };
 // Only change code below this line
 console.log(dog.name, dog.numLegs);
@@ -17,11 +17,11 @@ console.log(dog.name, dog.numLegs);
 //Test3
 
 let dog = {
-  name: 'Spot',
-  numLegs: 4,
-  sayLegs: function () {
-    return `This dog has ${dog.numLegs} legs.`;
-  },
+    name: 'Spot',
+    numLegs: 4,
+    sayLegs: function () {
+        return `This dog has ${dog.numLegs} legs.`;
+    },
 };
 
 dog.sayLegs();
@@ -29,11 +29,11 @@ dog.sayLegs();
 //Test4
 
 let dog = {
-  name: 'Spot',
-  numLegs: 4,
-  sayLegs: function () {
-    return 'This dog has ' + this.numLegs + ' legs.';
-  },
+    name: 'Spot',
+    numLegs: 4,
+    sayLegs: function () {
+        return 'This dog has ' + this.numLegs + ' legs.';
+    },
 };
 
 dog.sayLegs();
@@ -41,27 +41,28 @@ dog.sayLegs();
 //Test5
 
 function Dog() {
-  this.name = 'Gremlin';
-  this.color = 'dark';
-  this.numLegs = 4;
+    this.name = 'Gremlin';
+    this.color = 'dark';
+    this.numLegs = 4;
 }
 
 //Test6
 
 function Dog() {
-  this.name = 'Rupert';
-  this.color = 'brown';
-  this.numLegs = 4;
+    this.name = 'Rupert';
+    this.color = 'brown';
+    this.numLegs = 4;
 }
+
 // Only change code below this line
 let hound = new Dog();
 
 //Test7
 
 function Dog(name, color) {
-  this.name = name;
-  this.color = color;
-  this.numLegs = 4;
+    this.name = name;
+    this.color = color;
+    this.numLegs = 4;
 }
 
 let terrier = new Dog('terrier', 'dark');
@@ -69,7 +70,7 @@ let terrier = new Dog('terrier', 'dark');
 //Test8
 
 function House(numBedrooms) {
-  this.numBedrooms = numBedrooms;
+    this.numBedrooms = numBedrooms;
 }
 
 // Only change code below this line
@@ -81,8 +82,8 @@ myHouse instanceof House;
 //Test9
 
 function Bird(name) {
-  this.name = name;
-  this.numLegs = 2;
+    this.name = name;
+    this.numLegs = 2;
 }
 
 let canary = new Bird('Tweety');
@@ -90,14 +91,14 @@ let ownProps = [];
 // Only change code below this line
 
 for (let property in canary) {
-  if (canary.hasOwnProperty(property)) ownProps.push(property);
+    if (canary.hasOwnProperty(property)) ownProps.push(property);
 }
 console.log(ownProps);
 
 //Test10
 
 function Dog(name) {
-  this.name = name;
+    this.name = name;
 }
 
 Dog.prototype.numLegs = 4;
@@ -108,7 +109,7 @@ let beagle = new Dog('Snoopy');
 //Test11
 
 function Dog(name) {
-  this.name = name;
+    this.name = name;
 }
 
 Dog.prototype.numLegs = 4;
@@ -121,11 +122,11 @@ let prototypeProps = [];
 // Only change code below this line
 
 for (let property in beagle) {
-  if (beagle.hasOwnProperty(property)) {
-    ownProps.push(property);
-  } else {
-    prototypeProps.push(property);
-  }
+    if (beagle.hasOwnProperty(property)) {
+        ownProps.push(property);
+    } else {
+        prototypeProps.push(property);
+    }
 }
 
 console.log(ownProps);
@@ -134,57 +135,259 @@ console.log(prototypeProps);
 //Test12
 
 function Dog(name) {
-  this.name = name;
+    this.name = name;
 }
 
 // Only change code below this line
 function joinDogFraternity(candidate) {
-  if (candidate.constructor === Dog) {
-    return true;
-  } else {
-    return false;
-  }
+    if (candidate.constructor === Dog) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 //Test13
 
 function Dog(name) {
-  this.name = name;
+    this.name = name;
 }
 
 Dog.prototype = {
-  // Only change code below this line
-  numLegs: 4,
-  eat: function () {
-    console.log('Gav Gav');
-  },
-  describe: function () {
-    console.log(`My name is ${this.name}`);
-  },
+    // Only change code below this line
+    numLegs: 4,
+    eat: function () {
+        console.log('Gav Gav');
+    },
+    describe: function () {
+        console.log(`My name is ${this.name}`);
+    },
 };
 
 //Test14
 
+function Dog(name) {
+    this.name = name;
+}
+
+// Only change code below this line
+Dog.prototype = {
+    constructor: Dog,
+    numLegs: 4,
+    eat: function () {
+        console.log("nom nom nom");
+    },
+    describe: function () {
+        console.log("My name is " + this.name);
+    }
+};
+
 //Test15
+
+function Dog(name) {
+    this.name = name;
+}
+
+let beagle = new Dog("Snoopy");
+
+// Only change code below this line
+
+Dog.prototype.isPrototypeOf(beagle)
 
 //Test16
 
+function Dog(name) {
+    this.name = name;
+}
+
+let beagle = new Dog("Snoopy");
+
+Dog.prototype.isPrototypeOf(beagle);  // yields true
+
+// Fix the code below so that it evaluates to true
+Object.prototype.isPrototypeOf(Dog.prototype);
+
 //Test17
+
+function Cat(name) {
+    this.name = name;
+}
+
+Cat.prototype = {
+    constructor: Cat,
+
+};
+
+function Bear(name) {
+    this.name = name;
+}
+
+Bear.prototype = {
+    constructor: Bear,
+
+};
+
+function Animal() {
+}
+
+Animal.prototype = {
+    constructor: Animal,
+    eat: function () {
+        console.log("nom nom nom");
+    }
+};
 
 //Test18
 
+function Animal() { }
+
+Animal.prototype = {
+    constructor: Animal,
+    eat: function() {
+        console.log("nom nom nom");
+    }
+};
+
+// Only change code below this line
+
+let duck = Object.create(Animal.prototype); // Change this line
+let beagle= Object.create(Animal.prototype); // Change this line
+
 //Test19
+
+function Animal() { }
+
+Animal.prototype = {
+    constructor: Animal,
+    eat: function() {
+        console.log("nom nom nom");
+    }
+};
+
+function Dog() { }
+
+// Only change code below this line
+
+Dog.prototype = Object.create(Animal.prototype)
+let beagle = new Dog("gavgav");
+beagle.eat()
 
 //Test20
 
+function Animal() { }
+function Bird() { }
+function Dog() { }
+
+Bird.prototype = Object.create(Animal.prototype);
+Dog.prototype = Object.create(Animal.prototype);
+
+// Only change code below this line
+Bird.prototype.constructor = Bird
+Dog.prototype.constructor = Dog
+
+
+let duck = new Bird();
+let beagle = new Dog();
+
 //Test21
+
+function Animal() { }
+Animal.prototype.eat = function() { console.log("nom nom nom"); };
+
+function Dog() { }
+
+// Only change code below this line
+
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
+
+Dog.prototype.bark = function() {
+    console.log('Woof!')
+}
+
+
+// Only change code above this line
+
+let beagle = new Dog();
 
 //Test22
 
+function Bird() { }
+
+Bird.prototype.fly = function() { return "I am flying!"; };
+
+function Penguin() { }
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
+
+// Only change code below this line
+
+Penguin.prototype.fly = function() {
+    return "Alas, this is a flightless bird.";
+};
+
+// Only change code above this line
+
+let penguin = new Penguin();
+console.log(penguin.fly());
+
 //Test23
+
+let bird = {
+    name: "Donald",
+    numLegs: 2
+};
+
+let boat = {
+    name: "Warrior",
+    type: "race-boat"
+};
+
+// Only change code below this line
+
+let glideMixin = function(obj) {
+    obj.glide = function() {
+        console.log("glide")
+    }
+}
+glideMixin(bird)
+bird.glide()
+glideMixin(boat)
+boat.glide()
 
 //Test24
 
+function Bird() {
+    let weight = 15;
+
+    this.getWeight = function() {
+        return weight
+    }
+}
+
+let duc = new Bird()
+duc.getWeight()
+
 //Test25
 
+    (function () {
+        console.log("A cozy nest is ready");
+    })()
+
 //Test26
+
+let funModule =(function() {
+    return {
+        isCuteMixin: function(obj) {
+            obj.isCute = function() {
+                return true;
+            };
+        },
+        singMixin: function(obj) {
+            obj.sing = function() {
+                console.log("Singing to an awesome tune");
+            };
+        }
+
+    }
+})()
